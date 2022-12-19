28c49e51fa03ac507dcbdeedc9f3aba9e5bdb6e5
cwher <cwher@outlook.com>
2022-12-20 01:24:27 +0800

    feat: add static check

--- src/simulator.py
+++ src/simulator.py
@@ -4,7 +4,8 @@ from typing import List
 import tqdm
 
 from component_class import MemoryType, Operator, RunTime
-from solution_class import Decision, DecisionPhase, DecisionType
+from solution_class import Decision, DecisionType, MemoryBlockType
+from utils import printError
 
 
 class Simulator:
@@ -16,6 +17,7 @@ class Simulator:
         2. we assume the underlying machine with two levels of hierarchical memory,
             the slow memory (e.g., NVMe) CAN NOT be directly used in computation,
             but it supports full-duplex loading & storing
+        3. we assume fast memory allocation & copy is immediate without any time elapse
 
     NOTE: for forward pass
         1. for simplicity, we DO NOT employ re-materialization
@@ -34,30 +36,181 @@ class Simulator:
         self.run_time = run_time
         self.computation_graph = computation_graph
 
+    def staticProcess(self, solution: List[Decision]):
+        # NOTE:
+        #   1. find proper sink operators for backward pass
+        #   2. sequential issues
+        #       forward decisions happen before backward decisions
+        #       optimize decisions happen after backward decisions
+        #       prune decisions happens before backward decisions
+        #   3. channel conflict/completeness issues
+        #       e.g., multiple forward of same channel in same operator
+        #       e.g., not all channels are forwarded
+        #   4. generate commit & purge decisions
+        #       commit: to simplify simulation,
+        #        for each forward/backward/optimize a commit is generated
+        #       purge: refer to "when to purge data"
+        #   5. sort decisions by wall_time
+        # TODO: prune is not implemented yet
+
+        @dataclasses.dataclass
+        class Interval:
+            start: float = 1e100
+            end: float = -1e100
+
+            def update(self, t: float):
+                self.start = min(self.start, t)
+                self.end = max(self.end, t)
+
+        forward_interval: Dict[Operator, Interval] = {}
+        backward_interval: Dict[Operator, Interval] = {}
+        optimize_interval: Dict[Operator, Interval] = {}
+        prune_interval: Dict[Operator, Interval] = {}
+        intervals = {
+            DecisionType.FORWARD: forward_interval,
+            DecisionType.BACKWARD: backward_interval,
+            DecisionType.OPTIMIZE: optimize_interval,
+            DecisionType.PRUNE: prune_interval
+        }
+
+        operators: Set[Operator] = set()
+        forward_channels: Dict[Operator, Set[int]] = {}
+        backward_channels: Dict[Operator, Set[int]] = {}
+        optimize_channels: Dict[Operator, Set[int]] = {}
+        pruned_channels: Dict[Operator, Set[int]] = {}
+        channels = {
+            DecisionType.FORWARD: forward_channels,
+            DecisionType.BACKWARD: backward_channels,
+            DecisionType.OPTIMIZE: optimize_channels,
+            DecisionType.PRUNE: pruned_channels
+        }
+
+        for t in solution:
+            printError(
+                t.decision_type == DecisionType.COMMIT or
+                t.decision_type == DecisionType.PURGE)
+            # TODO
+            printError(t.decision_type == DecisionType.PRUNE)
+            operators.add(t.operator)
+        printError(operators != set(self.computation_graph))
+
+        for op in operators:
+            for channel in channels.values():
+                channel[op] = set()
+            for interval in intervals.values():
+                interval[op] = Interval()
+
+        # Thing 3
+        for t in solution:
+            if t.decision_type in channels:
+                channel_ids = set(t.channel_ids)
+                type_channels = channels[t.decision_type]
+                printError(len(channel_ids & type_channels[t.operator]) > 0)
+                type_channels[t.operator].update(t.channel_ids)
+        for op in operators:
+            # fmt: off
+            printError(forward_channels[op] != set(range(op.num_input_channels)))
+            printError(not pruned_channels[op].issubset(set(range(op.num_output_channels))))
+            printError(backward_channels[op] != set(range(op.num_output_channels)) - pruned_channels[op])
+            printError(optimize_channels[op] != backward_channels[op])
+            # fmt: on
+
+        # Thing 2
+        # HACK: last forward: [t, t + 10]
+        #       last backward: [t + 5, t + 15]
+        #   this issue is left for runtime check
+        for t in solution:
+            if t.decision_type in intervals:
+                intervals[t.decision_type][t.operator].update(t.wall_time)
+        for op in operators:
+            # fmt: off
+            printError(forward_interval[op].end > backward_interval[op].start)
+            printError(backward_interval[op].end > optimize_interval[op].start)
+            printError(prune_interval[op].end > backward_interval[op].start)
+            # fmt: on
+
+        # Thing 4
+        # TODO: prune
+        commit_types = {
+            DecisionType.FORWARD,
+            DecisionType.BACKWARD,
+            DecisionType.OPTIMIZE
+        }
+        # is_last_[type]_commit
+        self.commit_type: Dict[Decision, bool] = {}
+        # (which_operator, memory_block ,chanel_ids)
+        self.purge_type: Dict[Decision, List[
+            Tuple[Operator, MemoryBlockType, List[int]]]] = {}
+        for t in solution:
+            if t.decision_type in commit_types:
+                self.commit_type[t] = t.wall_time == \
+                    intervals[t.decision_type][t.operator].end
+        # HACK: reference counting (per memory type per channel)
+        output_ref_count: Dict[Operator, List[int]] = {}
+        grad_ref_count: Dict[Operator, List[int]] = {}
+        pass_grad_ref_count: Dict[Operator, List[int]] = {}
+        for op in operators:
+            # fmt: off
+            output_ref_count[op] = [len(op.succ_ops)] * op.num_output_channels
+            grad_ref_count[op] = [1] * op.num_output_channels
+            pass_grad_ref_count[op] = [len(op.pred_ops)] * op.num_input_channels
+            # fmt: on
+        # TODO: FIXME: sink and source ops
+        for t in solution:
+            self.purge_type[t] = []
+            if t.decision_type == DecisionType.FORWARD:
+                for op in t.operator.pred_ops:
+                    for channel_id in range(op.num_output_channels):
+                        output_ref_count[op][channel_id] -= 1
+                    if output_ref_count[op][0] == 0:
+                        self.purge_type[t].append(
+                            (op, MemoryBlockType.OUTPUT,
+                             list(range(op.num_output_channels))))
+            if t.decision_type == DecisionType.BACKWARD:
+                for op in t.operator.succ_ops:
+                    for channel_id in range(op.num_input_channels):
+                        pass_grad_ref_count[op][channel_id] -= 1
+                    if pass_grad_ref_count[op][0] == 0:
+                        self.purge_type[t].append(
+                            (op, MemoryBlockType.PASS_GRAD,
+                             list(range(op.num_input_channels))))
+                if t.wall_time == backward_interval[t.operator].end:
+                    self.purge_type[t].append(
+                        (t.operator, MemoryBlockType.INPUT,
+                         list(range(t.operator.num_input_channels))))
+            if t.decision_type == DecisionType.OPTIMIZE:
+                for channel_id in t.channel_ids:
+                    grad_ref_count[t.operator][channel_id] -= 1
+                self.purge_type[t].append(
+                    (t.operator, MemoryBlockType.GRAD, t.channel_ids))
+
+        # Thing 1
         self.source_op: List[Operator] = []
         self.sink_op: List[Operator] = []
-        for op in computation_graph:
+        for op in self.computation_graph:
             if len(op.pred_ops) == 0:
                 self.source_op.append(op)
-            if len(op.succ_ops) == 0:
+                # NOTE: input data are stored in slow memory
+                op.input_locations = \
+                    [MemoryType.SLOW] * op.num_input_channels
+
+            def isSink(op: Operator) -> bool:
+                for succ_op in op.succ_ops:
+                    if len(backward_channels[succ_op]) > 0:
+                        return False
+                return True
+            # NOTE: if all successors are pruned, then it is a sink operator
+            if isSink(op):
                 self.sink_op.append(op)
 
-    def run(self, solution: List[Decision]):
-        last_computing = 0.0
-        last_loading = 0.0
-        last_storing = 0.0
-        current_memory = 0.0
-        forward_done = False
+        # Thing 5
+        self.solution = sorted(solution, key=lambda x: x.wall_time)
 
-        # TODO: drop data
-        try:
-            for decision in tqdm.tqdm(solution):
-                if not forward_done \
-                        and self.checkForwardDone():
-                    forward_done = True
-                    print("===== Forward Pass Done =====")
-                    print("Current Time: {:.2f} s".format(last_computing))
-                    print("Current Memory: {:.2f} MB".format(current_memory))
+    def dynamicSim(self):
+        # NOTE:
+        #   1. generate commit & purge decisions
+        #   2. simulate all decisions
+        # TODO
 
                 if decision.decision_phase == DecisionPhase.FORWARD:
                     if decision.decision_type == DecisionType.COMPUTE:
