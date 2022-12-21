d5626e8adfb8f1fdb73fa127b63784e31511aad9
cwher <cwher@outlook.com>
2022-12-21 19:59:32 +0800

    feat: update demo

--- src/demo.py
+++ src/demo.py
@@ -35,16 +35,21 @@ if __name__ == "__main__":
                     param_size=5, input_size=5, output_size=1,
                     grad_size=5, pass_grad_size=5,
                     num_input_channels=5, num_output_channels=1)
-    op_A.init(pred_ops=[], succ_ops=[op_B, op_C])
-    op_B.init(pred_ops=[op_A], succ_ops=[op_D])
-    op_C.init(pred_ops=[op_A], succ_ops=[op_D])
-    op_D.init(pred_ops=[op_B, op_C], succ_ops=[])
+    op_A.init(pred_ops=[], succ_ops=[op_B, op_C],
+              output_channel_accuracy=[0.05] * op_A.num_output_channels)
+    op_B.init(pred_ops=[op_A], succ_ops=[op_D],
+              output_channel_accuracy=[0.05] * op_B.num_output_channels)
+    op_C.init(pred_ops=[op_A], succ_ops=[op_D],
+              output_channel_accuracy=[0.05] * op_C.num_output_channels)
+    op_D.init(pred_ops=[op_B, op_C], succ_ops=[],
+              output_channel_accuracy=[0.05] * op_D.num_output_channels)
 
     computation_graph = [op_A, op_B, op_C, op_D]
 
     run_time = RunTime(memory_limit=50,
                        cross_level_bandwidth_read=2,
-                       cross_level_bandwidth_write=1)
+                       cross_level_bandwidth_write=1,
+                       target_accuracy=0.8)
 
     # Let's compose a naive solution
     decisions = []
@@ -81,12 +86,26 @@ if __name__ == "__main__":
                                 memory_block=memory_blocks[i],
                                 channel_ids=channel_ids_list[i])
             decisions.append(decision)
-            current_wall_time += 10
+            current_wall_time += 8
+
+    # NOTE: prune
+    decisions.append(Decision(wall_time=current_wall_time,
+                              decision_type=DecisionType.PRUNE,
+                              operator=computation_graph[0],
+                              memory_block=None,
+                              channel_ids=[0, 1]))
+    for op in computation_graph[1:-1]:
+        decisions.append(Decision(wall_time=current_wall_time,
+                                  decision_type=DecisionType.PRUNE,
+                                  operator=op,
+                                  memory_block=None,
+                                  channel_ids=[0]))
+
     # NOTE: backward phase
     #   1. allocate gradient data
     #   2. allocate pass gradient data
     #   3. backward & optimize
-    for op in reversed(computation_graph):
+    for op in reversed(computation_graph[1:]):
         decision_types = [
             DecisionType.ALLOCATE,
             DecisionType.ALLOCATE,
@@ -99,33 +118,35 @@ if __name__ == "__main__":
             None,
             None
         ]
+        offset = 0 if op == computation_graph[-1] else 1
         channel_ids_list = [
             list(range(op.num_output_channels)),
-            list(range(op.num_input_channels)),
+            list(range(1, op.num_input_channels)),
             list(range(op.num_output_channels)),
             list(range(op.num_output_channels))
         ]
         for i in range(4):
-            if len(op.pred_ops) == 0 and \
+            if op != computation_graph[-1] and \
                     memory_blocks[i] == MemoryBlockType.PASS_GRAD:
                 continue
             decision = Decision(wall_time=current_wall_time,
                                 decision_type=decision_types[i],
                                 operator=op,
                                 memory_block=memory_blocks[i],
-                                channel_ids=channel_ids_list[i])
+                                channel_ids=channel_ids_list[i][offset:])
             decisions.append(decision)
-            current_wall_time += 15
+            current_wall_time += 12
     # NOTE: write back parameters
-    for op in reversed(computation_graph):
+    for op in reversed(computation_graph[1:]):
+        offset = 0 if op == computation_graph[-1] else 1
         decision = Decision(
             wall_time=current_wall_time,
             decision_type=DecisionType.STORE,
             operator=op,
             memory_block=MemoryBlockType.PARAM,
-            channel_ids=list(range(op.num_output_channels)))
+            channel_ids=list(range(op.num_output_channels))[offset:])
         decisions.append(decision)
-        current_wall_time += 10
+        current_wall_time += 6
 
     simulator = Simulator(run_time, computation_graph)
     simulator.staticAnalysis(decisions)
